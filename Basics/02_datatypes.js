782b7add201e7d0c25069f6d7140cb7254b8b512
shriyansh2305 <114873594+shriyansh2305@users.noreply.github.com>
2024-02-19 14:25:19 +0000

    forgot to add Lines

---
+++ Basics/02_datatypes.js
@@ -0,0 +1,24 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3 + 3); // we are using nodejs, not browser
+
+console.log(3 
+    + 
+    3); // code readability should be high
+
+console.log("shriyansh");
+// number => 2 power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value => if a server is unable to send temperature value then insteead of sending 0, it should send null => no any value came
+// undefined => value is not assigned
+// symbol => unique
+
+// object
+
+console.log(typeof "kittu");
+console.log(typeof null);  // object 
+console.log(typeof undefined); // undefined
+
+
